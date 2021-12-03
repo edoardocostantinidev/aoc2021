2dae603befb32f83ffa7bd39bfcf2bebd968ad27
Edoardo <edoardo.costantini@prima.it>
2021-12-03 19:07:24 +0100

    day3

--- day3/day3.js
+++ day3/day3.js
@@ -1,14 +1,50 @@
 const fs = require('fs');
 function main() {
-    const binNums = getBinaryNumbersFromFile('input.txt');
+    const bitArrays = getBinaryArraysFromFile('input.txt');
+    const transposedBitArrays = bitArrays[0].map((_, colIndex) => bitArrays.map(row => row[colIndex]));
+    const mostCommonBits = getMostCommonBits(bitArrays);
+    const leastCommonBits = getLeastCommonBits(bitArrays);
 
-    const gammaRate = convertByteArrayToInt(getGammaRate(binNums));
-    const epsilonRate = convertByteArrayToInt(getEpsilonRate(binNums));
+    const gammaRate = convertByteArrayToInt(mostCommonBits);
+    const epsilonRate = convertByteArrayToInt(leastCommonBits);
+    console.log(`most common bits: ${mostCommonBits}`);
+    console.log(`least common bits: ${leastCommonBits}`);
+
+    const oxygenGeneratorRatingArray = filterByBitCriteria(0, transposedBitArrays, true);
+    const co2ScrubberRatingArray = filterByBitCriteria(0, transposedBitArrays, false);
+    const oxygenGeneratorRating = convertByteArrayToInt(oxygenGeneratorRatingArray);
+    const co2ScrubberRating = convertByteArrayToInt(co2ScrubberRatingArray);
+    const lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating;
     const consumption = gammaRate * epsilonRate;
-    console.log(consumption)
+    console.log(`Gamma rate: ${gammaRate}`);
+    console.log(`Epsilon rate: ${epsilonRate}`);
+    console.log(`Consumption: ${consumption}`);
+    console.log(`O2 Generator Rating: ${oxygenGeneratorRating}`);
+    console.log(`Co2 Scrubber Rating: ${co2ScrubberRating}`);
+    console.log(`Life support rating: ${lifeSupportRating}`);
+}
+function filterByBitCriteria(index, bitArrays, most) {
+
+    if (bitArrays.length === 1) {
+        return bitArrays[0];
+
+    }
+    let newBitArrays = [];
+    const t = bitArrays[0].map((_, c) => bitArrays.map(r => r[c]));
+    const commonBits = most ? getMostCommonBits(t) : getLeastCommonBits(t);
+
+    const commonBit = commonBits[index];
+
+    for (let i = 0; i < bitArrays.length; i++) {
+        if (bitArrays[i][index] === commonBit)
+            newBitArrays.push(bitArrays[i]);
+    }
+
+
+    return filterByBitCriteria(++index, newBitArrays, most);
 }
 
-function getGammaRate(binaryNumbers) {
+function getMostCommonBits(binaryNumbers) {
     let gammaRate = [];
     for (const binNums of binaryNumbers) {
         let gamma = 0;
@@ -18,13 +54,15 @@ function getGammaRate(binaryNumbers) {
             else gamma--;
 
         }
-
-        gammaRate.push(gamma > 0 ? 1 : 0);
+        if (gamma === 0)
+            gammaRate.push(1);
+        else
+            gammaRate.push(gamma > 0 ? 1 : 0);
     }
     return gammaRate;
 }
 
-function getEpsilonRate(binaryNumbers) {
+function getLeastCommonBits(binaryNumbers) {
     let epsilonRate = [];
     for (const binNums of binaryNumbers) {
         let epsilon = 0;
@@ -33,17 +71,20 @@ function getEpsilonRate(binaryNumbers) {
                 epsilon++;
             else epsilon--;
         }
-        epsilonRate.push(epsilon < 0 ? 1 : 0);
+        if (epsilon === 0)
+            epsilonRate.push(0);
+        else
+            epsilonRate.push(epsilon < 0 ? 1 : 0);
     }
     return epsilonRate;
 
 }
 
 // [[all first bits], [all second bits],... [all nth bits]] 
-function getBinaryNumbersFromFile(fileName) {
+function getBinaryArraysFromFile(fileName) {
     const data = fs.readFileSync(fileName, 'utf8');
     const lines = data.split('\n');
-    const binNums = [];
+    const bitArrays = [];
     let bitLenght = lines[0].length
     for (let i = 0; i < bitLenght; i++) {
         let bitArray = [];
@@ -51,9 +92,9 @@ function getBinaryNumbersFromFile(fileName) {
             const element = parseInt(lines[j][i]);
             bitArray.push(element);
         }
-        binNums.push(bitArray);
+        bitArrays.push(bitArray);
     }
-    return binNums;
+    return bitArrays;
 }
 
 // convert byte array to int 
