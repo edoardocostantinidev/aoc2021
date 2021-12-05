7a8ea33c9f99b9ffcf48f8d225f9b6bb5b62cc88
Edoardo <edoardo.costantini@prima.it>
2021-12-05 16:15:57 +0100

    wip day5

---
+++ day5/day5.js
@@ -0,0 +1,143 @@
+const { count } = require('console');
+const fs = require('fs');
+const input = fs.readFileSync('test.txt', 'utf8');
+const x1s = [];
+const x2s = [];
+const y1s = [];
+const y2s = [];
+
+for (const line of input.split('\n')) {
+    const regex = /(\d{1,3})/gm;
+    const matches = line.match(regex);
+    x1s.push(parseInt(matches[0]));
+    y1s.push(parseInt(matches[1]));
+    x2s.push(parseInt(matches[2]));
+    y2s.push(parseInt(matches[3]));
+}
+
+const maxX = Math.max(...x1s, ...x2s) + 1;
+const maxY = Math.max(...y1s, ...y2s) + 1;
+console.log(maxX, maxY);
+const matrix = [];
+
+for (let x = 0; x < maxX; x++) {
+    let row = [];
+    for (let y = 0; y < maxY; y++) {
+        row.push(0);
+    }
+    matrix.push(row);
+}
+
+for (let i = 0; i < x1s.length; i++) {
+    const x1 = x1s[i];
+    const y1 = y1s[i];
+    const x2 = x2s[i];
+    const y2 = y2s[i];
+    console.log(x1, y1, x2, y2);
+    if (y1 == y2) {
+        const difference = x2 - x1;
+        if (difference > 0) {
+            //go to right
+            for (let x = difference; x >= 0; x--) {
+                matrix[y1][x1 + x] += 1;
+            }
+        } else if (difference < 0) {
+            //go to left
+            for (let x = difference; x <= 0; x++) {
+                matrix[y1][x1 + x] += 1;
+            }
+
+        }
+    }
+    else if (x1 == x2) {
+        const difference = y2 - y1;
+        if (difference > 0) {
+            //go down
+            for (let y = difference; y >= 0; y--) {
+                matrix[y1 + y][x1] += 1;
+            }
+        } else if (difference < 0) {
+            //go up
+            for (let y = difference; y <= 0; y++) {
+                matrix[y1 + y][x1] += 1;
+            }
+        }
+    } else if (x1 == y1 && x2 == y2) {
+        // go diagonal
+        const difference = x2 - x1;
+        if (difference > 0) {
+            //go down-right
+            for (let x = difference; x >= 0; x--) {
+                matrix[y1 + x][x1 + x] += 1;
+            }
+        } else if (difference < 0) {
+            //go up-left
+            for (let x = difference; x <= 0; x++) {
+                matrix[y1 - x][x1 - x] += 1;
+            }
+        }
+    }
+    else if (x1 == y2 && x2 == y1) {
+        const differenceX = x2 - x1;
+        const differenceY = y2 - y1;
+        if (differenceX > 0 && differenceY > 0) {
+            // go down right
+            for (let x = 0; x <= Math.abs(differenceX); x++) {
+                for (let y = 0; y <= Math.abs(differenceY); y++) {
+                    if (x == y) {
+                        matrix[y1 + y][x1 + x] += 1;
+                    }
+                }
+            }
+        } else if (differenceX < 0 && differenceY > 0) {
+            // go down left
+            for (let x = 0; x <= Math.abs(differenceX); x++) {
+                for (let y = 0; y <= Math.abs(differenceY); y++) {
+                    if (x == y) {
+                        matrix[y1 + y][x1 - x] += 1;
+                    }
+                }
+            }
+        }
+        else if (differenceX > 0 && differenceY < 0) {
+            // go up right
+            for (let x = 0; x <= Math.abs(differenceX); x++) {
+                for (let y = 0; y <= Math.abs(differenceY); y++) {
+                    if (x == y) {
+                        matrix[y1 - y][x1 + x] += 1;
+                    }
+                }
+            }
+        }
+        else if (differenceX < 0 && differenceY < 0) {
+            // go up left
+            for (let x = 0; x <= Math.abs(differenceX); x++) {
+                for (let y = 0; y <= Math.abs(differenceY); y++) {
+                    if (x == y) {
+                        matrix[y1 - y][x1 - x] += 1;
+                    }
+                }
+            }
+        }
+        else if (Math.abs(differenceX) == Math.abs(differenceY)) {
+            // go diagonal
+            for (let x = 0; x <= Math.abs(differenceX); x++) {
+                for (let y = 0; y <= Math.abs(differenceY); y++) {
+                    if (x == y) {
+                        matrix[y1 + y][x1 + x] += 1;
+                    }
+                }
+            }
+        }
+    }
+}
+function countMatrix(matrix) {
+    let count = 0;
+    for (let r = 0; r < matrix.length; r++) {
+        const row = matrix[r];
+        console.log(...row);
+        count += row.filter((a) => a >= 2).length;
+    }
+    return count;
+}
+console.log(countMatrix(matrix));
