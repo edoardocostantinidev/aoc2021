f2493769f426888f687db46906f1273ce0231e9e
Edoardo <edoardo.costantini@prima.it>
2021-12-09 11:24:58 +0100

    omg

--- day8/day8.js
+++ day8/day8.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-let input = fs.readFileSync('test.txt', 'utf8');
-input = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
+let input = fs.readFileSync('input.txt', 'utf8');
 let count = 0;
 for (const line of input.split('\n')) {
     const splitted = line.split('\|');
@@ -27,87 +26,82 @@ function getMappings(patterns) {
         'abcdfg'
     ];
     const newPairs = originalPairs.map((_) => '.');
-    for (const pattern of patterns) {
+    for (let i = 0; i < patterns.length; i++) {
+        const pattern = sortString(patterns[i]);
         const length = pattern.length;
-        const sortedPattern = sortString(pattern);
         switch (length) {
             case 2:
-                newPairs[1] = sortedPattern;
+                newPairs[1] = pattern;
                 break;
             case 3:
-                newPairs[7] = sortedPattern;
+                newPairs[7] = pattern;
                 break;
             case 4:
-                newPairs[4] = sortedPattern;
+                newPairs[4] = pattern;
                 break;
             case 7:
-                newPairs[8] = sortedPattern;
+                newPairs[8] = pattern;
                 break;
             default:
                 break;
         }
     }
-    const one = newPairs[1];
-    const four = newPairs[4];
-    const seven = newPairs[7];
 
-    for (const pattern of patterns) {
+    for (let i = 0; i < patterns.length; i++) {
+        const pattern = sortString(patterns[i]);
         const length = pattern.length;
-        const sortedPattern = sortString(pattern);
-        switch (length) {
-            case 6:
-                if (isCorrect(sortedPattern, four)) {
-                    newPairs[0] = sortedPattern;
-                } else if (isCorrect(sortedPattern, seven)) {
-                    newPairs[6] = sortedPattern;
-                } else {
-                    newPairs[9] = sortedPattern;
-                }
-
-            default:
-                break;
-        }
-    }
-
-    for (const pattern of patterns) {
-        const length = pattern.length;
-        const sortedPattern = sortString(pattern);
         switch (length) {
             case 5:
-                if (isCorrect(sortedPattern, one)) {
-                    newPairs[5] = sortedPattern;
-                } else if (isCorrect(sortedPattern, four)) {
-                    newPairs[3] = sortedPattern;
-                } else {
-                    newPairs[2] = sortedPattern;
+                if (intersections(pattern, newPairs[7]) === 3) {
+                    newPairs[3] = pattern;
+                    break;
                 }
-
+                else if (intersections(pattern, newPairs[4]) === 3) {
+                    newPairs[5] = pattern;
+                    break;
+                }
+                else {
+                    newPairs[2] = pattern;
+                    break;
+                }
+            case 6:
+                if (intersections(pattern, newPairs[4]) === 4) {
+                    newPairs[9] = pattern;
+                    break;
+                }
+                else if (intersections(pattern, newPairs[7]) === 3) {
+                    newPairs[0] = pattern;
+                    break;
+                }
+                else {
+                    newPairs[6] = pattern;
+                    break;
+                }
             default:
                 break;
         }
     }
-
     for (let i = 0; i < newPairs.length; i++) {
         mappings[newPairs[i]] = i;
     }
     return mappings;
 }
 
-
-function isCorrect(sortedPattern, numbersArray) {
-    //check if all elements of numbersArrays are all a subset of sortedPattern
-    for (const number of numbersArray) {
-        if (!sortedPattern.includes(number)) {
-            return false;
+function intersections(str1, str2) {
+    let count = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if (str2.includes(str1[i])) {
+            count++;
         }
     }
-    return true;
+    return count;
 }
+
 
 function getNumber(mappings, digits) {
     let exponent = 0;
     let count = 0;
-    for (let i = digits.length - 1; i > 0; i--) {
+    for (let i = digits.length - 1; i >= 0; i--) {
         const string = sortString(digits[i]);
         const element = mappings[string];
         if (element === undefined) {
